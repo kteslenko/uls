7c31b31b0c0bae119c11f2b3ab0b238a2fc0db49
Konstantin Teslenko <kostya5779025@gmail.com>
2023-03-21 13:04:39 +0200

    Add config header

---
+++ inc/config.h
@@ -0,0 +1,43 @@
+#pragma once
+
+typedef enum {
+    FORMAT_SINGLECOLUMN, // -1
+    FORMAT_MULTICOLUMN,  // -C
+    FORMAT_STREAM,       // -m
+    FORMAT_LONG,         // -l
+} t_format;
+
+typedef enum {
+    TIME_MODIFICATION,  // default
+    TIME_STATUS_CHANGE, // -c
+    TIME_ACCESS,        // -u
+} t_time_type;
+
+typedef enum {
+    SORT_NAME, // default
+    SORT_TIME, // -t
+    SORT_SIZE, // -S
+    SORT_NONE, // -f
+} t_sort_type;
+
+typedef enum {
+    IGNORE_HIDDEN, // default
+    IGNORE_DOTS,   // -A
+    IGNORE_NONE,   // -a
+} t_ignore_type;
+
+typedef struct {
+    t_format format;
+    t_time_type time_type;
+    t_sort_type sort_type;
+    t_ignore_type ignore_type;
+    bool recursive;           // -R
+    bool colorize;            // -G
+    bool classify;            // -F
+    bool slash;               // -p
+    bool sort_reverse;        // -r
+    bool human_readable;      // -h
+    bool access_control_list; // -e
+    bool complete_time_info;  // -T
+    bool extended_attributes; // -@
+} t_config;
